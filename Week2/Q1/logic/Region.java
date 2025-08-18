1b9922b1ded7090fe2f0615afcadbb62f1875c57
Burapha Thanataweerat <6738133021@student.chula.ac.th>
2025-08-18 15:50:40 +0700

    Debug Q2 Week1 and start of Q2

---
+++ Week2/Q1/logic/Region.java
@@ -0,0 +1,66 @@
+package HW.Week2.Q1.logic;
+import java.util.*;
+
+public class Region {
+	private String name;
+	private ArrayList<Player> playerList;
+	private ArrayList<Quest> questList;
+	
+	public Region(String name) {
+		setName(name);
+		playerList = new ArrayList<Player>();
+		questList = new ArrayList<Quest>();
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		if(name.isBlank()) {
+			this.name = "Nowhere";
+		}
+		else {
+			this.name = name;
+		}
+	}
+	
+	public ArrayList<Player> getPlayerList(){
+		return playerList;
+	}
+	
+	public void setPlayerList(ArrayList<Player> playerList) {
+		this.playerList = playerList;
+	}
+	
+	public ArrayList<Quest> getQuestList(){
+		return questList;
+	}
+	
+	public void setQuestList(ArrayList<Quest> questList) {
+		this.questList = questList;
+	}
+	
+	public int getPlayerCount() {
+		return playerList.size();
+	}
+	
+	public double getRegionRank() {
+		double total = 0;
+		double count = 0;
+		for(Player p : playerList) {
+			total += p.getRank();
+			count++;
+		}
+		return total/count;
+	}
+	
+	public ArrayList<Quest> getAvailableQuest(Player viewer){
+		ArrayList<Quest> availableQuest = new ArrayList<Quest>();
+		for(Quest q : questList) {
+			if(q != viewer.getCurrentQuest() && q.getStatus() == AVAILABLE) {continue;}
+			if(q.getStatus)
+		}
+	}
+	
+}
