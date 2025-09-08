9e060efddcd55eb9099358fb1fb1f421e73f46d8
Your Name <peeburapha@gmail.com>
2025-09-08 16:25:43 +0700

    Q1

---
+++ Week4/Q1/DListNode.java
@@ -0,0 +1,17 @@
+package HW.Week4.Q1;
+
+public class DListNode {
+	char data;
+	DListNode nextNode, previousNode;
+
+	DListNode(char data) {
+		this(data, null, null);
+	}
+
+	DListNode(char theElement, DListNode n, DListNode p) {
+		data = theElement;
+		nextNode = n;
+		previousNode = p;
+	}
+
+}
