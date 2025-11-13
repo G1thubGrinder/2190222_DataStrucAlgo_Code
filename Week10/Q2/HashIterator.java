98b7d2590fbdf4fb821c2825e895b4dc912974c9
Burapha Thanataweerat <6738133021@student.chula.ac.th>
2025-11-13 13:30:00 +0700

    Update HashIterator.java

--- Week10/Q2/HashIterator.java
+++ Week10/Q2/HashIterator.java
@@ -1,5 +1,74 @@
-package HW.Week10.Q2;
+public class HashIterator implements Iterator {
 
-public class HashIterator {
+	OpenAddressing h; // the associated hash table
+	int currentPos;   // position in the table's array that is currently marked.
+	int temp;
+	
+	//Create an iterator that marks the leftmost actual data in the hash table.
+	//Assume actual data are not 0 and DELETED.
+	//If there are no actual data in the table, set currentPos to -1.
+	public HashIterator(OpenAddressing o) {
+		h = o;
+		int i = 0;
+		for (; i < o.array.length; i++) {
+			if (o.array[i] != 0 && o.array[i] != OpenAddressing.DELETED) {
+				currentPos = i;
+				break;
+			}
+		}
+		if (i >= o.array.length) {
+			currentPos = -1;
+		}
+	}
+	
+	@Override
+	public boolean hasNext() {
+		if(currentPos >= h.array.length - 1 || currentPos == -1) return false;
+		for(int i = currentPos + 1; i < h.array.length; ++i) {
+			if(h.array[i] != 0 && h.array[i] != OpenAddressing.DELETED) {
+				temp = i;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean hasPrevious() {
+		if(currentPos <= 0) return false;
+		for(int i = currentPos - 1; i >= 0; i--) {
+			if(h.array[i] != 0 && h.array[i] != OpenAddressing.DELETED) {
+				temp = i;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public int next() throws Exception {
+		if(this.hasNext()) {
+			currentPos = temp;
+			return h.array[currentPos];
+		}
+		else throw new Exception();
+	}
+
+	@Override
+	public int previous() throws Exception {
+		if(this.hasPrevious()) {
+			int ans = currentPos;
+			currentPos = temp;
+			return h.array[ans];
+		}
+		else throw new Exception();
+	}
+
+	@Override
+	public void set(int value) {
+		// does not do anything,
+		//because it will break hash table definition
+
+	}
 
 }
